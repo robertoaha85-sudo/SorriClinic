31123e02b1d8581daa66598d1a9076e1b10c4055
robertoaha85-sudo <robertoaha85@gmail.com>
2026-02-25 23:22:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+  return {
+    plugins: [react(), tailwindcss()],
+    base: './',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
-  build: {
-    outDir: 'dist',
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
   }
 })
